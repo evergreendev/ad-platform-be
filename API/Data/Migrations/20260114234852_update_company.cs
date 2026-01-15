01a9976292c33d42480618505afc1031544815ff
Joe simpson <joe@egmrc.com>
2026-01-15 10:18:34 -0700

    Remove `AssociatedCompany`, `LegacyBillingContact`, and `LegacyPrimaryContact` columns from `company` table via migrations

---
+++ API/Data/Migrations/20260114234852_update_company.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_company : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssociatedCompany",
+                table: "company");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssociatedCompany",
+                table: "company",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ API/Data/Migrations/20260115165821_remove_company_properties.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_company_properties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LegacyBillingContact",
+                table: "company");
+
+            migrationBuilder.DropColumn(
+                name: "LegacyPrimaryContact",
+                table: "company");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LegacyBillingContact",
+                table: "company",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LegacyPrimaryContact",
+                table: "company",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+    }
+}
