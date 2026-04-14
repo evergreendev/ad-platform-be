e7a16039a87e78a35b2e0f4d1d415ff6d176abd0
Joe <joseph.simpson@jsdesdev.com>
2026-04-14 13:35:10 -0600

    Add CampaignsController with route stubs

---
+++ API/Controllers/CampaignsController.cs
@@ -0,0 +1,29 @@
+﻿using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+/*[Authorize] todo add back in after testing*/
+public class CampaignsController(): ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateCampaign(Campaign campaign)
+    {
+        return Ok();
+    }
+    
+    [HttpGet]
+    public IActionResult GetCampaigns()
+    {
+        return Ok();
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetCampaignById(int id)
+    {
+        return Ok();
+    }
+}
