534c337ff5c7f802f494888acbce8e0b034d0ec3
Joe <joseph.simpson@jsdesdev.com>
2026-05-14 15:27:42 -0600

    Add `is_default` column to `integration_connection` table with migration

---
+++ API/Data/Migrations/20260514152916_update_integration_connection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_integration_connection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_default",
+                table: "integration_connection",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_default",
+                table: "integration_connection");
+        }
+    }
+}
